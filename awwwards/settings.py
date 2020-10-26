5a621c535d64869384f77d779d78b39d7b0ffd43
Brian-Muchera <mucherabrian2@gmail.com>
2020-10-26 17:37:38 +0300

    changes

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -148,8 +148,6 @@ LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'index'
 
-SIGNUP_REDIRECT_URL = 'index'
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
